7eb2955f1e3b4e66749fce97214f1d5e370e1a18
Todd Carlson <todd.carlson3030@me.com>
2019-04-24 06:33:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,4 +10,12 @@ function forLoop(array) {
   return array;
 }
 
+function whileLoop(number) {
+  let countdown = 100;
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+  return 'done';
+}
+
 
