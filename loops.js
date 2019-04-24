fc626c811d618cc2a2d5c31a86f549377f4c1ede
Todd Carlson <todd.carlson3030@me.com>
2019-04-24 06:10:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(array) {
     if(i === 1) {
       array.push("I am 1 strange loop.")
       return array 
-    } else if (i <=25) {
+    } else if (i === 0 || i <= 25) {
       array.push("I am ${i} strange loops.")
       return array
     } 
